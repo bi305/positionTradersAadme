6da05c3bea28fa8641c6324e8cfa472019a591ac
usman <akhtarusman716@gmail.com>
2023-04-14 16:24:16 +0500

    update buttons

--- src/pages/courses.js
+++ src/pages/courses.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Layout } from "@/Layout";
-import { Container, Grid, Typography, Box } from "@mui/material";
+import { Container, Grid, Typography, Box ,Button} from "@mui/material";
 import Head from "next/head";
 
 const courses = () => {
@@ -28,9 +28,9 @@ const courses = () => {
               <Box
                 display="flex"
                 justifyContent="center"
-                sx={{ fontWeight: "900" }}
+               
               >
-                <Typography component="h2" variant="h2" color="inherit">
+                <Typography component="h2" variant="h2" color="inherit"  sx={{ fontWeight: "600" ,py:"65px"}}>
                   Crypto Currency
                 </Typography>
               </Box>
@@ -41,27 +41,27 @@ const courses = () => {
                   display: "flex",
                   justifyContent: "center",
                   alignItems: "end",
-                  height: "700px",
+                  height: "600px",
                 }}
               >
                 <Box component="ul">
                   <Box component="li">
-                    <Typography sx={{ fontSize: "26px", fontWeight: "400" }}>
+                    <Typography sx={{ fontSize: "28px", fontWeight: "400" }}>
                       WEBINAR OF MARKET <br /> UPDATES
                     </Typography>
                   </Box>
                   <Box component="li">
-                    <Typography sx={{ fontSize: "26px", fontWeight: "400" }}>
+                    <Typography sx={{ fontSize: "28px", fontWeight: "400" }}>
                       SIGNALS ALERT ROOM
                     </Typography>
                   </Box>
                   <Box component="li">
-                    <Typography sx={{ fontSize: "26px", fontWeight: "400" }}>
+                    <Typography sx={{ fontSize: "28px", fontWeight: "400" }}>
                       CAHTROOM
                     </Typography>
                   </Box>
                   <Box component="li">
-                    <Typography sx={{ fontSize: "26px", fontWeight: "400" }}>
+                    <Typography sx={{ fontSize: "28px", fontWeight: "400" }}>
                       24/7 HELP
                     </Typography>
                   </Box>
@@ -70,10 +70,32 @@ const courses = () => {
               <Grid
                 item
                 md={12}
-                sx={{ display: "flex", justifyContent: "center", mt: 10 }}
+                sx={{ display: "flex", justifyContent: "center", mt: 22 }}
               >
-                <Box  sx={{width:"200px" }}>
-                  <button>hello</button>
+                <Box sx={{}} >
+                  <Button 
+				  sx={{backgroundImage:"linear-gradient(180deg, rgba(251, 156, 247, 0.19) 9.67%, #FF00C7 100%) !important"
+				,width:"300px"
+				,borderRadius:"35px"
+				,color:"#fff"
+				,fontSize:"20px"
+				,px:"16px"
+				}}>
+					$400 Lifetime
+					</Button>
+                </Box>
+				<Box sx={{}} >
+                  <Button 
+				  sx={{backgroundImage:"linear-gradient(180deg, rgba(251, 156, 247, 0.19) 9.67%, #FF00C7 100%) !important"
+				,width:"300px"
+				,borderRadius:"35px"
+				,color:"#fff"
+				,fontSize:"20px"
+				,px:"16px"
+				,ml:"20px"
+				}}>
+					$200/Monthly
+					</Button>
                 </Box>
               </Grid>
             </Grid>
